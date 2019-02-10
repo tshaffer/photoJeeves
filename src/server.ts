b58f18eea92790858973915860c4ccda384a3f4f
Ted Shaffer <ted@brightsign.biz>
2019-02-10 05:14:47 -0800

    minor cleanup.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,4 @@
-// import express from 'express';
-// const app = express();
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-// app.listen(8088, () => console.log('Example app listening on port 8088!'));
-
 import app from './app';
-// const PORT = 8088;
 const PORT = 3000;
 
 app.listen(PORT, () => {

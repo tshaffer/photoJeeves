928af86f66b70db14c13dcbfbd98eabdc861bdeb
Ted Shaffer <ted@brightsign.biz>
2019-02-10 06:08:30 -0800

    reorganization / cleanup

--- src/app.ts
+++ src/app.ts
@@ -9,32 +9,37 @@ const mongoDB = 'mongodb://ted:photoTed0524@ds014648.mlab.com:14648/photos';
 
 class App {
 
-    public app: express.Application;
-    public route: Routes = new Routes();
+  public app: express.Application;
+  public route: Routes = new Routes();
 
-    constructor() {
+  constructor() {
 
-        this.app = express();
-        this.config();
-        this.route.routes(this.app);
+    this.app = express();
+    this.config();
+    this.route.routes(this.app);
 
-        mongoose.connect(mongoDB);
-        mongoose.Promise = global.Promise;
-        var db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    mongoose.connect(mongoDB);
+    mongoose.Promise = global.Promise;
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-        console.log('end of constructor');
-    }
+    console.log('end of constructor');
+  }
 
-    private config(): void {
+  private config(): void {
 
-        // support application/json type post data
-        this.app.use(bodyParser.json());
+    this.app.set("port", process.env.PORT || 3000);
 
-        // support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+    // support application/json type post data
+    this.app.use(bodyParser.json());
 
+    // support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    // view setup
+    this.app.set("views", path.join(__dirname, "../views"));
+    this.app.set("view engine", "pug");
+  }
 }
 
 export default new App().app;
--- src/controllers/syncerController.ts
+++ src/controllers/syncerController.ts
@@ -6,9 +6,9 @@ import { Request, Response } from "express";
 export function startSync(request: Request, response: Response, next?: any): any {
   console.log(__dirname);
   console.log(process.cwd());
-  // response.sendFile('../views/syncer.html');
 
-  const baseDir: string = process.cwd();
-  const pagePath = path.join(baseDir, 'src/views/syncer.html');
+  // const baseDir: string = process.cwd();
+  // const pagePath = path.join(baseDir, '/views/syncer.html');
+  const pagePath = path.join(__dirname, '../../views/syncer.html');
   response.sendFile(pagePath);
 }

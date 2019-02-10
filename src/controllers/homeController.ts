109bd00e73758013773e1425c28bb772d7a3dee7
Ted Shaffer <ted@brightsign.biz>
2019-02-10 07:39:40 -0800

    home page updates

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from "express";
 
+import MediaItem from '../models/mediaItem';
+
 export function start(request: Request, response: Response) {
-  response.render('home'); 
+
+  // response.render('home');
+
+  MediaItem.count({}, (err, count) => {
+    if (err) { debugger }
+    MediaItem.find({ 'downloaded': true }, 'fileName', (err, downloadedMediaItems) => {
+      if (err) debugger;
+      response.render('home',
+        {
+          mediaItemCount: count,
+          mediaItemsDownloadedCount: downloadedMediaItems.length,
+          mediaItemsPendingDownloadCount: count - downloadedMediaItems.length,
+        });
+    });
+  });
+
 }

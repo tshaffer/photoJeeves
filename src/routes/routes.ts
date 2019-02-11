4a070b009ae3c5cc56b406ee4828694d18cea21f
Ted Shaffer <ted@brightsign.biz>
2019-02-10 16:53:20 -0800

    route handling cleanup

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,3 @@
-import { Request, Response, response } from 'express';
 import express from 'express';
 
 import { getCode, handleAuthCallback } from '../controllers/oauth2Controller';
@@ -12,27 +11,9 @@ export class Routes {
   }
 
   createRoutes(app: express.Application) {
-
-    // express.Router().get('/', getCode);
-    app.route('/')
-      .get((request: Request, response: Response) => {
-        getCode(request, response);
-      });
-
-    // express.Router().get('/authCallback.*', handleAuthCallback);
-    app.route('/authCallback.*')
-      .get((request: Request, response: Response) => {
-        handleAuthCallback(request, response);
-      });
-
-    express.Router().get('/home', start);
-
-    // the following does not work - figure out why
-    // express.Router().get('/checkForNewContent', checkForContent);
-
-    app.route('/checkForNewContent')
-      .get((request: Request, response: Response) => {
-        checkForContent(request, response);
-      });
+    app.get('/', getCode);
+    app.get('/authCallback.*', handleAuthCallback);
+    app.get('/home', start);
+    app.get('/checkForNewContent', checkForContent);
   }
 }

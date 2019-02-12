ab56abfc901e015ca288b45871a11ffd82ce83db
Ted Shaffer <ted@brightsign.biz>
2019-02-11 17:44:24 -0800

    cleanup

--- src/controllers/events.ts
+++ src/controllers/events.ts
@@ -12,38 +12,6 @@ export function eventHandler(request: Request, response: Response) {
   });
 
   sseResponse = response;
-
-  setTimeout(() => {
-    sendFirstResponse();
-    // console.log(sseResponse);
-    // sseResponse.write('data: {"flight": "I768", "state": "landing"}');
-    // sseResponse.write("\n\n");
-  }, 10000);
-
-  setTimeout(() => {
-    sendSecondResponse();
-    // console.log(sseResponse);
-    // sseResponse.write('data: {"pizza": "I768", "state": "pepperoni"}');
-    // sseResponse.write("\n\n");
-  }, 20000);
-
-  console.log(sseResponse);
-}
-
-// export function getSseResponse(): Response {
-//   return sseResponse;
-// }
-
-function sendFirstResponse() {
-  console.log(sseResponse);
-  sseResponse.write('data: {"flight": "I768", "state": "landing"}');
-  sseResponse.write("\n\n");
-}
-
-function sendSecondResponse() {
-  console.log(sseResponse);
-  sseResponse.write('data: {"pizza": "I768", "state": "pepperoni"}');
-  sseResponse.write("\n\n");
 }
 
 export function postSseResponse(responseContents: any) {
@@ -51,16 +19,8 @@ export function postSseResponse(responseContents: any) {
   const responseData = 'data: ' + JSON.stringify(responseContents);
   console.log(responseData);
 
-  // sseResponse.write('data: {"pizza": "I768", "state": "sausage"}');
-  sseResponse.write(responseData);
-  sseResponse.write("\n\n");
-  // console.log(sseResponse);
-  // console.log('postSseResponse');
-  // if (sseResponse) {
-  //   console.log('invoke sseResponse.write');
-  //   console.log(JSON.stringify(responseContents));
-  //   sseResponse.write(JSON.stringify(responseContents));
-  //   sseResponse.write('\n\n');
-  //   sseResponse.write('\n\n');
-  // }
+  if (sseResponse) {
+    sseResponse.write(responseData);
+    sseResponse.write("\n\n");
+  }
 }

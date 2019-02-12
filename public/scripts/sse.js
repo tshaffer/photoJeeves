f48a048fbb3cdfa48b60e7c18ba6318b4b9ed91c
Ted Shaffer <ted@brightsign.biz>
2019-02-11 17:54:21 -0800

    updated element on screen based on sse. YEAH!!

--- public/scripts/sse.js
+++ public/scripts/sse.js
@@ -3,6 +3,17 @@ console.log('hello, I am sse');
 var evtSource = new EventSource('events'); 
 
 evtSource.onmessage = function(e) {
-  console.log('received message');
+  console.log('received message: ');
   console.log(e);
+
+  console.log('data is: ');
+  console.log(e.data);
+
+  messageData = JSON.parse(e.data);
+  console.log('message data: ');
+  console.log(messageData);
+
+  var pLocalMediaContent  = document.getElementById('localMediaContent');
+  console.log(pLocalMediaContent)
+  pLocalMediaContent.innerHTML = 'Number of local media items: ' + messageData.downloadedMediaItemCount.toString();
 }

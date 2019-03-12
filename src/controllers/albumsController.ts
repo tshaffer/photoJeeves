0a7cb2726585bae89f974f4b40c5350be374bd46
Ted Shaffer <ted@brightsign.biz>
2019-03-11 18:07:30 -0700

    ongoing development

--- src/controllers/albumsController.ts
+++ src/controllers/albumsController.ts
@@ -4,16 +4,68 @@ import {
   getGoogleAlbums,
 } from '../utilities/googleInterface';
 import {
-  GoogleAlbum,
+  getDbAlbums,
+} from '../utilities/dbInterface';
+
+import {
+  GoogleAlbum, DbAlbum,
 } from '../types';
 
-export function showAlbumsStatus(request: Request, response: Response) {
-  getGoogleAlbums().then((googleAlbums: GoogleAlbum[]) => {
-    console.log('showAlbumsStatus');
-    console.log(googleAlbums);
+interface CompositeAlbum {
+  id: string;
+  googleTitle: string;
+  googlePhotoCount: number;
+  dbTitle: string;
+  dbPhotoCount: number;
+  hdTitle?: string;
+  hdPhotoCount?: number;
+}
 
+export function showAlbumsStatus(request: Request, response: Response) {
+
+  const promises: Array<Promise<any>> = [];
+
+  promises.push(getGoogleAlbums());
+  promises.push(getDbAlbums());
+
+  Promise.all(promises).then((albumStatusResults: any[]) => {
+    console.log(albumStatusResults);
+    const googleAlbums: GoogleAlbum[] = albumStatusResults[0];
+    const dbAlbums: DbAlbum[] = albumStatusResults[1];
+
+    const albumsById: any = {};
+    googleAlbums.forEach((googleAlbum: GoogleAlbum) => {
+      const allAlbum: CompositeAlbum = {
+        id: googleAlbum.googleAlbumId,
+        googleTitle: googleAlbum.title,
+        googlePhotoCount: googleAlbum.mediaItemsCount,
+        dbTitle: '',
+        dbPhotoCount: 0,
+      };
+      albumsById[googleAlbum.googleAlbumId] = allAlbum;
+    });
+
+    dbAlbums.forEach((dbAlbum: DbAlbum) => {
+      if (albumsById.hasOwnProperty(dbAlbum.googleId)) {
+        const compositeAlbum: CompositeAlbum = albumsById[dbAlbum.googleId];
+        compositeAlbum.dbTitle = dbAlbum.title;
+        compositeAlbum.dbPhotoCount = dbAlbum.mediaItemIds.length;
+        albumsById[dbAlbum.googleId] = compositeAlbum;
+      }
+      else {
+        console.log('No matching google album for dbAlbum: ', dbAlbum.title);
+      }
+    });
+
+    const allAlbums: CompositeAlbum[] = [];
+    for (const albumId in albumsById) {
+      if (albumsById.hasOwnProperty(albumId)) {
+        const compositeAlbum = albumsById[albumId];
+        allAlbums.push(compositeAlbum);
+      }
+    }
     response.render('albums', {
-      albums: googleAlbums,
+      albums: allAlbums,
     });
   });
 }
---
+++ src/utilities/dbInterface.ts
@@ -0,0 +1,33 @@
+import Album from '../models/album';
+
+import {
+  DbAlbum,
+  PhotoMetadata,
+  DbMediaItem,
+  GoogleAlbum,
+  GoogleMediaItem,
+  AlbumWithDifferences,
+} from '../types';
+
+export function getDbAlbums(): Promise<DbAlbum[]> {
+  console.log('begin: retrieve downloadedAlbums from mongoose');
+  const query = Album.find({});
+  return query.exec().then((results: any) => {
+    const dbAlbums: DbAlbum[] = results.map((result: any) => {
+      return {
+        googleId: result.id,
+        title: result.title,
+        mediaItemIds: [getDbMediaItemIds(result.mediaItemIds)],
+      };
+    });
+    return Promise.resolve(dbAlbums);
+  });
+}
+
+function getDbMediaItemIds(dbMediaItemIds: any[]): string[] {
+  return dbMediaItemIds.map((mediaItemId: any) => {
+    return mediaItemId.toString();
+  });
+}
+
+
